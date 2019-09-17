e3e05605e0f709a4feeb1c1844378c148c727843
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-17 19:25:27 +0530

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+#include "constants.h"
+#include "loaders.h"
+#include "shade.h"
+#include "validators.h"
+
+#ifdef _GCC_VERSION_LESS_THAN_8_
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#elif _GCC_VERSION_MORE_THAN_8_
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#endif
+
+struct credentials
+{
+    std::string user, pwd, newpwd, confirm;
+};
+
+class xCredentials
+{
+    std::string user, pwd;
+public:
+    bool userisAdmin();
+    void __init__(std::string, std::string);
+    bool operator==(const xCredentials&);
+    friend std::ostream& operator<<(std::ostream&, const xCredentials&);
+    friend std::istream& operator>>(std::istream&,  xCredentials&);
+    bool _Check(bool);
+    void _Save();
+    bool _isUserUnique();
+    bool _Delete();
+    void _Modify(std::string, std::string);
+};
