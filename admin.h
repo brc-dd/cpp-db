8f3bbcc0f80fcac130607f07fe22d837f43f116c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-20 16:01:38 +0530

    Create admin.h

---
+++ admin.h
@@ -0,0 +1,13 @@
+#pragma once
+ 
+#include <bits/stdc++.h>
+
+#include "rlutil.h"
+#include "constants.h"
+#include "loaders.h"
+#include "users.h"
+
+using namespace std::literals::chrono_literals;
+
+void AdminChoiceProcessor(bool);
+void AdminMenuLoader(bool);
