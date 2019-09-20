ceb8ce17bb0be3844008654169483e4a47a464f0
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-20 16:18:47 +0530

    Update users.cpp

--- users.cpp
+++ users.cpp
@@ -1,70 +1,71 @@
-void AdminMenuLoader(bool);
+#include "users.h"
 
 void user_create()
 {
     credentials new_user=caller1(false);
-    string str="Creating";
-    Load0(str, 1, 1, false);cout<<'\b';
+    std::string str="Creating";
+    Load0(str, 1, 1, false);
+    std::cout<<'\b';
     for(int i=0; i<4; i++)
     {
-        cout<<'.';
-        sleep_until(system_clock::now() + seconds(1));
+        std::cout<<'.';
+        std::this_thread::sleep_for(1s);
     }
     if(new_user.confirm==new_user.pwd)
     {
-        xDefault.construct(new_user.user, new_user.pwd);
+        xDefault.__init__(new_user.user, new_user.pwd);
         if(xDefault._isUserUnique())
         {
             xDefault._Save();
             str="New user account successfully created!";
             Load0(str, 0, 1);
-            _getch();
+            getch();
         }
         else
         {
             str="Error! The user already exists!";
             Load0(str, 0, 1);
-            _getch();
+            getch();
         }
     }
     else
     {
         str="Error! The passwords do not match!";
         Load0(str, 0, 1);
-        _getch();
+        getch();
     }
-    xDefault.userisAdmin=true;
+    xDefault.userisAdmin()=true;
     AdminMenuLoader(true);
 }
 
 void user_delete()
 {
-    string query;
-    cout<<"\nEnter the name of the user you want to delete: ";
+    std::string query;
+    std::cout<<"\nEnter the name of the user you want to delete: ";
     cin>>query;
-    xDefault.construct(query, string(" "));
-    if(xDefault.userisAdmin)
-        cout<<"\nSorry admin account cannot be deleted!";
+    xDefault.__init__(query, std::string(" "));
+    if(xDefault.userisAdmin())
+        std::cout<<"\nSorry admin account cannot be deleted!";
     else if(xDefault._Delete())
-        cout<<"\nUser account successfully deleted!";
+        std::cout<<"\nUser account successfully deleted!";
     else
-        cout<<"\nNo such user exists!";
-    _getch();
-    xDefault.userisAdmin=true;
+        std::cout<<"\nNo such user exists!";
+    getch();
+    xDefault.userisAdmin()=true;
     AdminMenuLoader(true);
 }
 
 void user_modify()
 {
     credentials modified_user=caller1(true, true);
-    string str="Working on it";
+    std::string str="Working on it";
     Load0(str, 1, 1, false);
     for(int i=0; i<4; i++)
     {
-        cout<<'.';
+        std::cout<<'.';
         sleep_until(system_clock::now() + seconds(1));
     }
-    xDefault.construct(modified_user.user, modified_user.pwd);
+    xDefault.__init__(modified_user.user, modified_user.pwd);
     if(xDefault._Check(false))
     {
         if(modified_user.confirm==modified_user.newpwd)
@@ -72,21 +73,21 @@ void user_modify()
             xDefault._Modify(modified_user.user, modified_user.newpwd);
             str="User's credentials successfully modified!";
             Load0(str, 0, 1);
-            _getch();
+            getch();
         }
         else
         {
             str="Error! The passwords do not match!";
             Load0(str, 0, 1);
-            _getch();
+            getch();
         }
     }
     else
     {
         str="User's original credentials are not correct!";
         Load0(str, 0, 1);
-        _getch();
+        getch();
     }
-    xDefault.userisAdmin=true;
+    xDefault.userisAdmin()=true;
     AdminMenuLoader(true);
 }
