dd68279f8dfb4f827435d7a24cafb6776c7e5e79
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-20 16:02:53 +0530

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,73 +1,75 @@
-void AdminChoiceProcessor(bool bipass)
+#include "admin.h"
+
+void AdminChoiceProcessor(bool bypass)
 {
-    char choice, ch;
+    char ch;
     do
     {
-        if(not bipass)
+        if(not bypass)
         {
-            cout<<"\n\nEnter your choice: ";
-            cin>>choice;
-            cout<<endl;
-            switch(choice)
+            std::cout<<"\n\nEnter your choice: ";
+            std::cin>>ch;
+            std::cout<<'\n';
+            switch(ch)
             {
-            case '1':
-                user_create();
-                break;
-            case '2':
-                user_delete();
-                break;
-            case '3':
-                user_modify();
-                break;
-            case '4':
-                cout<<"\nFeature under development!";
-                break;
-            case '5':
-                return;
-                break;
-            default:
-                cout<<"\nWrong Choice!";
-                break;
+                case '1':
+                    user_create();
+                    break;
+                case '2':
+                    user_delete();
+                    break;
+                case '3':
+                    user_modify();
+                    break;
+                case '4':
+                    std::cout<<"\nFeature under development!";
+                    break;
+                case '5':
+                    return;
+                    break;
+                default:
+                    std::cout<<"\nWrong Choice!";
+                    break;
             }
         }
         else
         {
-            cout<<"\n\nContinue? (y/n) ";
-            cin.clear();cin.sync();
-            cin.get(ch);
+            std::cout<<"\n\nContinue? (y/n) ";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.get(ch);
             if(ch=='Y' or ch=='y')
-                bipass=false;
+                bypass=false;
             else break;
         }
     } while(true);
     if(ch!='Y' or ch!='y')
-        exit(0);
+        std::exit(0);
 }
 
-void AdminMenuLoader(bool bipass)
+void AdminMenuLoader(bool bypass)
 {
-    hidecursor();
+    rlutil::hidecursor();
     int i=0;
-    string str="Welcome to Admin Panel!";
-    if(not bipass)
+    std::string str="Welcome to Admin Panel!";
+    if(not bypass)
     {
         Load0(str, 0, 1);
-        sleep_until(system_clock::now()+seconds(2));
+        std::this_thread::sleep_for(1s);
         str="Loading";
         Load0(str, 1, 1, false);
         for(i=0; i<4; i++)
         {
-            cout<<'.';
-            sleep_until(system_clock::now() + seconds(1));
+            std::cout<<'.';
+            std::this_thread::sleep_for(1s);
         }
     }
-    system("cls");
+    rlutil::cls();
     str="Central Investigation Department";
-    cout<<endl;
+    std::cout<<'\n';
     Align0(str, -2);
-    cout<<endl;
-    for(i=0; i<console_width; i++) cout<<'*';
-    cout<<'\n';
+    std::cout<<'\n';
+    for(i=0; i<console_width; i++) std::cout<<'*';
+    std::cout<<'\n';
     for(i=1; i<=5; i++)
     {
         switch(i)
@@ -88,11 +90,11 @@ void AdminMenuLoader(bool bipass)
                 str="5. Return to main menu   ";
                 break;
         }
-        cout<<'*';
+        std::cout<<'*';
         Align0(str, (str.length()/2+1)-padding_horizontal, false);
-        cout<<setw((console_width-str.length())/2)<<'*'<<endl<<endl;
+        std::cout<<std::setw((console_width-str.length())/2)<<'*'<<"\n\n";
     }
-    for(i=0; i<console_width; i++) cout<<'*';
-    showcursor();
-    AdminChoiceProcessor(bipass);
+    for(i=0; i<console_width; i++) std::cout<<'*';
+    rlutil::showcursor();
+    AdminChoiceProcessor(bypass);
 }
