74eaf01dfd14bf36462d8742f0d00b187cb7894f
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-20 16:19:55 +0530

    Create users.h

---
+++ users.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+#include "callers.h"
+#include "credentials.h"
+#include "loaders.h"
+#include "admin.h"
+#include "rlutil.h"
+
+void user_create();
+void user_delete();
+void user_modify();
+extern void AdminMenuLoader(bool);
