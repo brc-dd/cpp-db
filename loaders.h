6e61d017c9090c7e722efbd3f8df8e4bf3b64899
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2019-09-20 16:15:32 +0530

    Update loaders.h

--- loaders.h
+++ loaders.h
@@ -4,8 +4,8 @@
 #include <windef.h>
 #include <windows.h>
 #include "constants.h"
+#include "rlutil.h"
 
-void ClearScreen();
 void SetWindow(int Width, int Height, bool enableScrollBar=false);
 void Align0(std::string str, int i, bool change_line=true);
 void Load0(std::string str, int k, int padding_change, bool change_line=true);
